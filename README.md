6bbb8516cd978dbaf7de8200dc0e4c4fd5b7d315
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-02-24 09:21:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables_OMalley_Lauren
+ 
---
+++ Variables_OMalley_Lauren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_OMalley_Lauren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
